1a38e9c6fa6089a88eefa18c6d4e4d1f3e8c3bad
Kelvin Wong <code@kelvinwong.ca>
2014-03-13 16:31:57 -0700

    Renderer

---
+++ likert_field/templatetags/likert_star_tools.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from six import string_types
+
+
+TAG_TEMPLATE = "<{tag} class='{tag_class}'{extra}>{inner}</{tag}>"
+
+star_set_keys = ['star', 'unlit', 'noanswer']
+
+
+def render_stars(num, max_stars, star_set):
+    """
+    Star renderer returns a HTML string of stars
+
+    If num is None or a blank string, it returns the unanswered tag
+
+    Otherwise, the returned string will contain num solid stars
+    followed by max_stars - num empty stars
+
+    If num > max_stars, render max_stars solid stars
+
+    star_set is a dictionary of strings with keys: star, unlit, noanswer
+    """
+    if num is None or (isinstance(num, string_types) and len(num) == 0):
+        return star_set['noanswer']
+
+    difference = max_stars - int(num)
+    if difference < 0:
+        num = max_stars
+        difference = 0
+
+    return ''.join(star_set['star'] * int(num) + star_set['unlit'] * difference)
