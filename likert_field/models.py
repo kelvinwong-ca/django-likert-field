b008d31bdfd7be2e78508cd7b78e311cc2370572
Kelvin Wong <code@kelvinwong.ca>
2014-03-14 02:23:45 -0700

    Field testing

--- likert_field/models.py
+++ likert_field/models.py
@@ -4,11 +4,13 @@ from __future__ import unicode_literals
 from six import string_types
 
 from django.db import models
+from django.utils.encoding import force_text, python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 import likert_field.forms as forms
 
 
+@python_2_unicode_compatible
 class LikertField(models.IntegerField):
     """A Likert field is simply stored as an IntegerField"""
     description = _('Likert item field')
@@ -18,14 +20,22 @@ class LikertField(models.IntegerField):
             kwargs['null'] = True
         super(LikertField, self).__init__(*args, **kwargs)
 
+    def __str__(self):
+        return "%s" % force_text(self.description)
+
     def get_prep_value(self, value):
-        """The field expects a number as a string (ie. '2').
-           Unscored fields are empty strings and are stored as NULL
+        """
+        Perform preliminary non-db specific value checks and conversions.
+
+        The field expects a number as a string (ie. '2'). Unscored fields are
+        empty strings and are stored as NULL
         """
         if value is None:
             return None
+
         if isinstance(value, string_types) and len(value) == 0:
             return None
+
         return int(value)
 
     def formfield(self, **kwargs):
--- test_suite/test_models.py
+++ test_suite/test_models.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models.fields import Field
 from django.test import SimpleTestCase
 
 from likert_field.models import LikertField
@@ -13,4 +14,30 @@ class LikertFieldTestCase(SimpleTestCase):
 
     def test_instantiation(self):
         item = LikertField()
-        self.assertTrue(len(item.description) > 0)
+        self.assertIsInstance(item, Field)
+
+    def test_(self):
+        item = LikertField()
+        print(item)
+
+    def test_get_prep_value_int(self):
+        item = LikertField()
+        for value in xrange(1, 50+1):
+            self.assertIsInstance(item.get_prep_value(value), int)
+
+    def test_get_prep_value_strings(self):
+        item = LikertField()
+        for value in xrange(1, 50+1):
+            self.assertIsInstance(item.get_prep_value(str(value)), int)
+
+    def test_get_prep_value_negative_int(self):
+        item = LikertField()
+        self.assertIsInstance(item.get_prep_value(-1), int)
+
+    def test_get_prep_value_empty_string(self):
+        item = LikertField()
+        self.assertTrue(item.get_prep_value('') is None)
+
+    def test_get_prep_value_null(self):
+        item = LikertField()
+        self.assertTrue(item.get_prep_value(None) is None)
