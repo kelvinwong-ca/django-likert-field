9f841dfd4bcba6991585ece4fbdd538620e3a329
Kelvin Wong <code@kelvinwong.ca>
2014-03-15 21:52:02 -0700

    decruft

--- test_suite/test_widgets.py
+++ test_suite/test_widgets.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.forms import widgets
 from django.test import SimpleTestCase
-from django.utils.six.moves import xrange
 
 from likert_field.widgets import LikertTextField
 
