746a6357d6195a08e2071b92336e410378b08ce6
juliandehne <julian.dehne@gmail.com>
2021-11-11 10:09:23 +0100

    Update models.py
    fixes error: ImportError: cannot import name 'python_2_unicode_compatible' from 'django.utils.encoding' (/usr/local/lib/python3.8/dist-packages/django/utils/encoding.py)
    when using django version > 3.

--- likert_field/models.py
+++ likert_field/models.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import force_text, python_2_unicode_compatible
+from django.utils.encoding import force_text
+from six import python_2_unicode_compatible
 from django.utils.six import string_types
 from django.utils.translation import ugettext_lazy as _
 
